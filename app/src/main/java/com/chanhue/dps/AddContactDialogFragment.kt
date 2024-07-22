59a2ceda7677cd2ff911572052061e3b6e388f91
seojw0124 <wjddn2000124@gmail.com>
2024-07-22 20:45:34 +0900

    add: AddContactDialogFragment 생성

--- app/src/main/java/com/chanhue/dps/AddContactDialogFragment.kt
+++ app/src/main/java/com/chanhue/dps/AddContactDialogFragment.kt
@@ -1,4 +1,18 @@
 package com.chanhue.dps
 
-class AddContactDialogFragment {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class AddContactDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setMessage("확인")
+            .setPositiveButton("확인") { _,_ -> }
+            .create()
+
+    companion object {
+        const val TAG = "AddContactDialogFragment"
+    }
 }
