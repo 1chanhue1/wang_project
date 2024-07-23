097458eaea7dd721927f31d37e85a36ca7543ae6
chanhue <chanhue467@gmail.com>
2024-07-23 23:24:33 +0900

    [model] modify data class

---
+++ app/src/main/java/com/chanhue/dps/model/MyProfile.kt
@@ -0,0 +1,8 @@
+package com.chanhue.dps.model
+
+data class MyProfile(
+    val name:String,
+    val phoneNumber:String,
+    val region:String,
+    val emergencyNumber:String
+    )
--- app/src/main/java/com/chanhue/dps/model/Owner.kt
+++ app/src/main/java/com/chanhue/dps/model/Owner.kt
@@ -8,8 +8,8 @@ import kotlinx.parcelize.Parcelize
 data class Owner(
 
     val id:Int,
-    val age:Int,
+    val name:String,
     val gender:Boolean, // 0 남자 1 여자
-    val nickname:String,
-    val phonenumber:String
+    val phoneNumber:String,
+    val region:String
 ):Parcelable
