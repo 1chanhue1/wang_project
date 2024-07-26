81c3b30c9aaec8ef4f675d56b7b1ac7ab7173747
chanhue <chanhue467@gmail.com>
2024-07-26 10:29:44 +0900

    [feature-mainpage] add ContactListFragment

--- app/src/main/java/com/chanhue/dps/ContactListFragment.kt
+++ app/src/main/java/com/chanhue/dps/ContactListFragment.kt
@@ -1,46 +1,42 @@
 package com.chanhue.dps
 
+import ContactAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.chanhue.dps.databinding.FragmentContactListBinding
+import com.chanhue.dps.model.Contact
+import com.chanhue.dps.model.ContactManager
+import com.chanhue.dps.model.Owner
+import com.chanhue.dps.model.PetProfile
 import com.chanhue.dps.ui.AddContactDialogFragment
+import com.chanhue.dps.viewmodel.ContactViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class ContactListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private var _binding: FragmentContactListBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val contactViewModel: ContactViewModel by activityViewModels()
+    private var isGridLayout = false // 기본은 리스트 레이아웃
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentContactListBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -48,11 +44,34 @@ class ContactListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initFloatingButton()
+        initLayoutToggleButton()
+
+        // ViewModel - LiveData
+        contactViewModel.contacts.observe(viewLifecycleOwner) { contacts ->
+            val adapter = ContactAdapter(contacts)
+            binding.recyclerViewContacts.adapter = adapter
+            setLayoutManager()
+        }
     }
 
     private fun initFloatingButton() {
         binding.ivContact.setOnClickListener {
             showDialog()
+        }
+    }
+
+    private fun initLayoutToggleButton() {
+        binding.toggleLayoutButton.setOnClickListener {
+            isGridLayout = !isGridLayout
+            setLayoutManager()
+        }
+    }
+
+    private fun setLayoutManager() {
+        binding.recyclerViewContacts.layoutManager = if (isGridLayout) {
+            GridLayoutManager(context, 2) // 2열로 세팅( 그리드 레이아웃)
+        } else {
+            LinearLayoutManager(context) // (리스트 레이아웃)
         }
     }
 
@@ -65,12 +84,6 @@ class ContactListFragment : Fragment() {
             setReorderingAllowed(true)
             addToBackStack(null)
             setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-//            setCustomAnimations(
-//                R.anim.dialog_slide_up,  // 프래그먼트가 나타날 때의 애니메이션
-//                R.anim.dialog_slide_down,  // 프래그먼트가 사라질 때의 애니메이션
-//                R.anim.dialog_slide_up,  // 백 스택에서 다시 나타날 때의 애니메이션
-//                R.anim.dialog_slide_down  // 백 스택에서 사라질 때의 애니메이션
-//            )
             add(android.R.id.content, dialogFragment)
         }
         DialogStateManager.setIsShowing(true)
@@ -82,15 +95,6 @@ class ContactListFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ContactListFragment().apply {
